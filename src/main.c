cf9dcc52eb4ea6829d7a17803526fc5317ebbe8d
ArturoCordoba <arturocv16@gmail.com>
2021-04-27 14:57:04 -0600

    Archivo de prueba de la biblioteca

--- src/main.c
+++ src/main.c
@@ -1,27 +1,33 @@
 #include <smgpio.h>
 
+#define output_pin_1 26
+#define output_pin_2 6
+#define input_pin 16
+
 int main() {
-    // Test the function to established the mode
-    pinMode(22, OUTPUT);
-    pinMode(16, INPUT);
+    // Test the function to establish the mode
+    pinMode(output_pin_1, OUTPUT);
+    pinMode(output_pin_2, OUTPUT);
+    pinMode(input_pin, INPUT);
+
+    // Blnk with 1 second frecuency, during 5 seconds
+    blink(output_pin_1, 1, 5);
+    sleep(5);
 
     // Test the digital write
-    digitalWrite(22, LOW);
-    digitalWrite(22, HIGH);
+    digitalWrite(output_pin_2, LOW);
+    digitalWrite(output_pin_2, HIGH);
 
     // Test the digital read
-    int value = digitalRead(22);
-
-    if(value != HIGH) {
-        printf("Error on digital read/write\n");
-        exit(1);
-    }
+    int value = digitalRead(input_pin);
+    printf("Value read on GPIO 16: %d\n", value);
 
     // Test the unexport
-    unexportGpio(16);
-    unexportGpio(22);
+    unexportGpio(output_pin_1);
+    unexportGpio(output_pin_2);
+    unexportGpio(input_pin);
 
-    printf("Library sm-gpio works as expected!\n");
+    printf("Library smgpio works as expected!\n");
 
     return 0;
 }
